a3a285d01f56e0cd5b27147e914464f1e8e84882
Tushar Pachouri <tusharpachouri001@gmail.com>
2024-04-23 01:07:10 +0530

    Add nodemailer package for email functionality

--- src/app.js
+++ src/app.js
@@ -25,12 +25,14 @@ app.use(cookieParser());
 import userRouter from "./routes/user.routes.js"
 import productRouter from "./routes/product.routes.js";
 import rentRouter from "./routes/rent.routes.js";
+import contactRouter from "./routes/contact.routes.js";
 
 
 //routes declarations :-
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/products", productRouter);
 app.use("/api/v1/rentals", rentRouter);
+app.use("/api/v1/contacts", contactRouter);
 
 
 export { app };
