2bdc993f8af3b6d863bb6850358080fa39c97ed0
Tushar Pachouri <tusharpachouri001@gmail.com>
2024-04-23 00:37:27 +0530

    Add endRental route and update rent.routes.js

--- src/routes/rent.routes.js
+++ src/routes/rent.routes.js
@@ -3,6 +3,8 @@ import {
   addRental,
   getRentals,
   getRentalsByLoggedInUser,
+  endRental,
+  getRentalsBySellerId,
 } from "../controllers/rent.controllers.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import { upload } from "../middlewares/multer.middleware.js";
@@ -12,5 +14,7 @@ const router = Router();
 router.post("/add/:productId", verifyJWT, addRental);
 router.get("/", getRentals);
 router.get("/user", verifyJWT, getRentalsByLoggedInUser);
+router.patch("/end/:id", verifyJWT, endRental);
+router.get("/seller/:id", getRentalsBySellerId);
 
 export default router;
